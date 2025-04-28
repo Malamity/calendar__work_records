d30a96154f17ecf788ed5d4ffc8a41b9bae6c940
michalszura <115370163+Malamity@users.noreply.github.com>
2025-04-28 20:16:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-from datetime import datetime, timedelta, UTC
+import datetime  # import datetime, timedelta, UTC
 
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+PL_Holidays = holidays.CountryHoliday('PL', years=datetime.datetime.now().year)
 
 
 def auth_google_calendar(scopes, credentials_path="credentials.json", token_path="token.json"):
@@ -31,40 +33,41 @@ def auth_google_calendar(scopes, credentials_path="credentials.json", token_path
 def add_event(
     service,
     summary,
-    location,
-    description,
     start_time,
     end_time,
     timezone="Europe/Warsaw",
-    attendees_emails=None,
 ):
     event = {
         "summary": summary,
-        "location": location,
-        "description": description,
         "start": {"dateTime": start_time.isoformat(), "timeZone": timezone},
         "end": {"dateTime": end_time.isoformat(), "timeZone": timezone},
-        "attendees": ([{"email": email} for email in attendees_emails] if attendees_emails else []),
-        "reminders": {
-            "useDefault": False,
-            "overrides": [
-                {"method": "email", "minutes": 24 * 60},
-                {"method": "popup", "minutes": 10},
-            ],
-        },
     }
     created_event = service.events().insert(calendarId="primary", body=event).execute()
     print(f"Event created: {created_event.get('htmlLink')}")
 
 
+def add_work_event(service, start_time):
+    end_time = start_time + datetime.timedelta(hours=7)
+    add_event(service, 'Praca', start_time, end_time)
+
+
 def check_event(service, max_results=10):
-    now = datetime.now(UTC).isoformat()
+    now = datetime.datetime.now(datetime.timezone.utc)
+    time_min = (now - datetime.timedelta(days=7)).isoformat()
+    time_max = now.isoformat()
+
+    # print(f'Time: {now}')
+    # print(f'Time: {time_min}')
+    # print(f'Time: {time_max}')
+
     print(f"Getting upcoming {max_results} events")
     events_result = (
         service.events()
         .list(
+            q='Praca',
             calendarId="primary",
-            timeMin=now,
+            timeMin=time_min,
+            timeMax=time_max,
             maxResults=max_results,
             singleEvents=True,
             orderBy="startTime",
@@ -74,26 +77,46 @@ def check_event(service, max_results=10):
 
     events = events_result.get("items", [])
 
-    if not events:
-        print("No upcoming events found")
-        return
+    print(f'Event count: {len(events)}')
+    return events
 
-    for event in events:
-        start = event["start"].get("dateTime", event["start"].get("date"))
-        print(f"{start} - {event.get('summary', 'No Title')}")
+
+def last_work_day(events: list[dict]):
+    last_day = ''
+    event = events[len(events) - 1]
+
+    for key, value in event.items():
+        if key == 'end':
+            last_day = str(value['dateTime'])[:10]
+
+    last_day = datetime.datetime.strptime(last_day, '%Y-%m-%d').date()
+    return last_day
 
 
 def main():
+    now_date = datetime.datetime.now().date()
     creds = auth_google_calendar(SCOPES)
     service = build("calendar", "V3", credentials=creds)
-    # summary = 'Test'
-    # location = 'Somewhere'
-    # description = 'TEST'
-    # start_time = datetime(2025, 4, 22, 10, 0)
-    # end_time = start_time + timedelta(hours=36)
+    events = check_event(service, max_results=15)
 
-    # add_event(service, summary, location, description, start_time, end_time)
-    # check_event(service, max_results=15)
+    last_day = last_work_day(events)
+    # print(f'Last day: {last_day}')
+
+    next_week_firstday = ''
+    next_week_lastday = ''
+
+    if last_day <= now_date:
+        next_week_firstday = last_day + datetime.timedelta(days=(8 - last_day.isoweekday()))
+
+    next_week_lastday = next_week_firstday + datetime.timedelta(days=4)
+    next_week_delta = (next_week_lastday - next_week_firstday).days
+
+    for i in range(next_week_delta + 1):
+        day = next_week_firstday + datetime.timedelta(days=i)
+        if day not in PL_Holidays:
+            day = str(day) + 'T08:00:00.000000'
+            day = datetime.datetime.strptime(day, '%Y-%m-%dT%H:%M:%S.%f')
+            add_work_event(service, day)
 
 
 if __name__ == "__main__":
